523d8a59ed325375202665f99d85c65417ea1205
luisfamaciase12-eng <luisfamaciase12@gmail.com>
2025-11-23 16:09:08 -0600

    modificacion de agenda.h por los valores de visual

--- src/agenda.h
+++ src/agenda.h
@@ -2,36 +2,39 @@
 #define __AGENDA_H__
 
 #define MAX_CONTACTOS 100
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
 
 enum TipoTelefono {CASA, MOVIL, OFICINA, OTRO};
 enum Mes{ENERO, FEBERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE};
 
-struct Persona{
+typedef struct{
     char nombre[30];
-    // apellido
-    // mes de nacimiento
-    // dia de nacimiento
-    // tipo contacto
-    // numero de telefono
-    // tipo de numero
-};
+    char apellido[30];
+    char telefono[20];
+    char tipo_tel[20];
+    int dia;
+    int mes;
+}Persona;
 
 typedef struct Persona Contacto;
 
 typedef struct Agenda{
-    Contacto contactos[MAX_CONTACTOS];
+    Contacto contactos[MAX];
     int num_contactos; //Lleva la cuenta de cuantos contactos están en la agenda
 } Agenda;
 
 
 void iniciar_agenda(Agenda *agenda);
 void agregar_contacto(Agenda *agenda, Contacto c);
-void imprimir_agenda(Agenda agenda);
 int buscar_contacto(Agenda *agenda, char *nombre);
 int buscar_contacto_x_telefono(Agenda *agenda, char telefono[]);
 void ordenar_contactos(Agenda *agenda);
 void ordenar_contactos_inv(Agenda *agenda);
-void mostrar_contacto(Contacto);
+void mostrar_contactos(Contacto);
 void leer_contacto(Contacto *c);
 void cargar_contactos(char *filename);
 void guardar_contactos(char *filename);
